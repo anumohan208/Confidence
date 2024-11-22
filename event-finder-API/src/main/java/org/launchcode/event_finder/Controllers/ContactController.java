c4bf6c7fd8f91aff280930b0881c2d40b874a389
anumohan208 <154403264+anumohan208@users.noreply.github.com>
2024-11-21 21:53:53 -0600

    mail_API changes

--- event-finder-API/src/main/java/org/launchcode/event_finder/Controllers/ContactController.java
+++ event-finder-API/src/main/java/org/launchcode/event_finder/Controllers/ContactController.java
@@ -5,7 +5,6 @@ import org.launchcode.event_finder.Repositories.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 
