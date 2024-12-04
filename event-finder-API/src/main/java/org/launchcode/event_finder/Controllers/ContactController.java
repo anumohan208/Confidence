9a26727c37d4ba6d850f21bcf2520caf2c1147ce
anumohan208 <154403264+anumohan208@users.noreply.github.com>
2024-12-03 22:51:03 -0600

    mail option in admindashboard-API changes

--- event-finder-API/src/main/java/org/launchcode/event_finder/Controllers/ContactController.java
+++ event-finder-API/src/main/java/org/launchcode/event_finder/Controllers/ContactController.java
@@ -1,10 +1,14 @@
 package org.launchcode.event_finder.Controllers;
 
 import org.launchcode.event_finder.Models.Contact;
+import org.launchcode.event_finder.Models.DTO.EmailRequest;
 import org.launchcode.event_finder.Repositories.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 
@@ -13,6 +17,9 @@ import java.util.List;
 public class ContactController {
     @Autowired
     private final ContactRepository contactRepository;
+
+    @Autowired
+    private JavaMailSender mailSender;
 
     public ContactController(ContactRepository contactRepository) {
         this.contactRepository = contactRepository;
@@ -26,5 +33,24 @@ public class ContactController {
     @GetMapping
     public List<Contact> getAllMessages() {
         return contactRepository.findAll();
+    }
+
+    @PostMapping("/send-email")
+    public ResponseEntity<String> sendEmail(@RequestBody EmailRequest emailRequest) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom("anumolpm0@gmail.com");
+            message.setTo(emailRequest.getRecipient());
+            message.setSubject(emailRequest.getSubject());
+            message.setText(emailRequest.getMessage());
+
+            // Send the email
+            mailSender.send(message);
+
+            return ResponseEntity.ok("Email sent successfully!");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).body("Failed to send email.");
+        }
     }
 }
